c74d76ffef0585fcd2a32cf6a8a1a82a75b44c6a
guilisboa07 <guilhermy.lisboa05@gmail.com>
2022-04-11 08:53:42 -0300

    Criação dos métodos de Adicionar e Excluir registros

--- boa-viagem-api/boa-viagem-api/src/main/java/br/com/etechoracio/boa_viagem/repository/GastoRepository.java
+++ boa-viagem-api/boa-viagem-api/src/main/java/br/com/etechoracio/boa_viagem/repository/GastoRepository.java
@@ -1,4 +1,4 @@
-package br.com.etechoracio.repository;
+package br.com.etechoracio.boa_viagem.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.etechoracio.boa_viagem.entity.Gasto;
 
